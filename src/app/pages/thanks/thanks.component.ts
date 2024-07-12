f2e2fb76518246c7f818d5d1bddc080d307c2e41
JoniMeichtry11 <joni.meichtry@gmail.com>
2024-07-12 12:21:18 -0300

    feat: add styles and configurations in thanks page

--- src/app/pages/thanks/thanks.component.ts
+++ src/app/pages/thanks/thanks.component.ts
@@ -1,12 +1,55 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, signal, Signal } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import confetti from 'canvas-confetti';
 
 @Component({
   selector: 'app-thanks',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './thanks.component.html',
   styleUrl: './thanks.component.scss'
 })
-export class ThanksComponent {
+export class ThanksComponent implements AfterViewInit {
+  selectedShipping: Signal<boolean> = signal(true);
+  count = 200;
+  defaults = {
+    origin: { y: 0.7 },
+  };
 
+  ngAfterViewInit(): void {
+    this.pushCelebrate()
+  }
+
+  pushCelebrate(){
+    this.celebrate(0.25, {
+      spread: 26,
+      startVelocity: 55,
+    });
+    this.celebrate(0.2, {
+      spread: 60,
+    });
+    this.celebrate(0.35, {
+      spread: 100,
+      decay: 0.91,
+      scalar: 0.8,
+    });
+    this.celebrate(0.1, {
+      spread: 120,
+      startVelocity: 25,
+      decay: 0.92,
+      scalar: 1.2,
+    });
+    this.celebrate(0.1, {
+      spread: 120,
+      startVelocity: 45,
+    });
+  }
+
+  celebrate(particleRatio: any, opts: any) {
+    confetti({
+      ...this.defaults,
+      ...opts,
+      particleCount: Math.floor(this.count * particleRatio),
+    });
+  }
 }
