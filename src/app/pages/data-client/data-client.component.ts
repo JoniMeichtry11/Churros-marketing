6965e06b894971cc0cf4b40cc8f15d93dd0150e0
JoniMeichtry11 <joni.meichtry@gmail.com>
2024-07-12 09:09:46 -0300

    feat: validate data client form

--- src/app/pages/data-client/data-client.component.ts
+++ src/app/pages/data-client/data-client.component.ts
@@ -1,3 +1,4 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
 import {
   FormBuilder,
@@ -5,23 +6,47 @@ import {
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-data-client',
   standalone: true,
-  imports: [ReactiveFormsModule, RouterLink],
+  imports: [ReactiveFormsModule, RouterLink, NgClass],
   templateUrl: './data-client.component.html',
   styleUrl: './data-client.component.scss',
 })
 export class DataClientComponent {
   clientForm: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor(private formBuilder: FormBuilder, private router: Router) {
     this.clientForm = this.formBuilder.group({
       name: ['', [Validators.required]],
       address: ['', [Validators.required]],
       aditionalInfoAddress: [''],
     });
+  }
+
+  get name() {
+    return this.clientForm.get('name');
+  }
+
+  get address() {
+    return this.clientForm.get('address');
+  }
+
+  onSubmit() {
+    if (this.clientForm.valid) {
+      let localStoragePurchase =
+        JSON.parse(localStorage.getItem('purchase') as string) || {};
+
+      const formData = this.clientForm.value;
+      localStoragePurchase = { ...localStoragePurchase, ...formData };
+
+      localStorage.setItem('purchase', JSON.stringify(localStoragePurchase));
+
+      this.router.navigate(['review']);
+    } else {
+      console.error('Form is invalid', this.clientForm.value);
+    }
   }
 }
