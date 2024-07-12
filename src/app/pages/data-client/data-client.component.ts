6844c2c3a7ab386168b87b2d81eb4c0696370d47
JoniMeichtry11 <joni.meichtry@gmail.com>
2024-07-11 23:05:26 -0300

    feat: styles data client component

--- src/app/pages/data-client/data-client.component.ts
+++ src/app/pages/data-client/data-client.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-data-client',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, RouterLink],
   templateUrl: './data-client.component.html',
-  styleUrl: './data-client.component.scss'
+  styleUrl: './data-client.component.scss',
 })
 export class DataClientComponent {
+  clientForm: FormGroup;
 
+  constructor(private formBuilder: FormBuilder) {
+    this.clientForm = this.formBuilder.group({
+      name: ['', [Validators.required]],
+      address: ['', [Validators.required]],
+      aditionalInfoAddress: [''],
+    });
+  }
 }
