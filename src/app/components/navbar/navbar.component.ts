4c3c6d5c127e66ba2690221c3cc5ce3097c9e9ef
JoniMeichtry11 <joni.meichtry@gmail.com>
2024-07-19 17:02:18 -0300

    feat: condition for show navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
 })
-export class NavbarComponent {}
+export class NavbarComponent implements OnInit{
+  showNavbar: boolean = true;
+  constructor(
+    private router: Router,
+  ) {}
+
+  ngOnInit() {
+    this.router.events.subscribe((event: any) => {
+      if (event.urlAfterRedirects) {
+        const currentRoute = event.urlAfterRedirects;
+        currentRoute === '/' ? this.showNavbar = true : this.showNavbar = false;
+      }
+    });
+  }
+}
